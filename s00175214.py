7a48290957146ee79d1e41019394ea965d31c3b3
Eoghan Spillane <Eoghan.Spillane@mycit.ie>
2020-12-26 20:38:23 +0000

    Q1 redone with everyone under the age of 50

--- s00175214.py
+++ s00175214.py
@@ -41,11 +41,9 @@ def cleanData1():
     df['relationship'] = df['relationship'].str.strip()
     df.dropna()
 
-
     df['Income'] = df['Income'].fillna(0)  # Turning Nan into the most Common Value
     df.loc[df['Income'].str.contains('<=50K', na=False), 'Income'] = 0
     df.loc[df['Income'].str.contains('>50K', na=False), 'Income'] = 1
-
 
     # print(df['education'].value_counts()) Most Common = HS-grad
     df['education'] = df['education'].fillna("HS-grad")  # Turning Nan into the most Common Value
@@ -55,7 +53,6 @@ def cleanData1():
         df.loc[df['education'] == y, 'education'] = x
         x += 1
 
-
     # print(df['workType'].value_counts())  # = Private
     df['workType'] = df['workType'].fillna("Private")  # Turning Nan into the most Common Value
     df['workType'] = df['workType'].str.replace('?', 'Other')
@@ -63,7 +60,6 @@ def cleanData1():
     for y in df['workType'].unique():
         df.loc[df['workType'] == y, 'workType'] = x
         x += 1
-
 
     # print(df['job'].value_counts())  # = Prof-specialty
     df['job'] = df['job'].fillna("Prof-specialty")  # Turning Nan into the most Common Value
@@ -85,7 +81,8 @@ def cleanData1():
 
 
 def task1():
-    df = cleanData1()
+    dfAll = cleanData1()
+    df = dfAll[dfAll['age'] < 50]
     # Machine Learning Stuff
     X = df[['education', 'workType', 'job']]  # Things to base the predictions around
     y = df[['Income']]  # Things to predict
@@ -97,16 +94,74 @@ def task1():
     print('Training: ', clf.score(X_train, y_train))
     print('Test: ', clf.score(X_test, y_test))
 
-    # Training at 33%: 0.7929584382098348
-    # Testing at 33%: 0.7865355056870581
+    # Training at 33%: 0.8097258229240317
+    # Testing at 33%: 0.8035405192761605
 
-    # Training at 95%: 0.8329238329238329
-    # Testing at 95%: 0.7717075063037434
+    # Training at 95%: 0.8343815513626834
+    # Testing at 95%: 0.7873138444567016
 
-    # When running the code, I noticed that each result has a +-0.25
+    """
+    Training at 33%: 0.8097258229240317
+    Testing at 33%: 0.8035405192761605
+
+    Training at 95%: 0.8343815513626834
+    Testing at 95%: 0.7873138444567016
+    
+    When running the code, I noticed that each result has a variance of +-0.03.
+    I saw this when running the program multiple times. regardless of testing at 0.95 or 0.333.
+    
+    """
+
+
+def cleanData2():
+    df = importFile()
+
+    # Removing WhiteSpace and endings
+    df['workType'] = df['workType'].str.strip()
+    df['education'] = df['education'].str.strip()
+    df['native-country'] = df['native-country'].str.strip()
+    df['job'] = df['job'].str.strip()
+    df['Income'] = df['Income'].str.strip(' \n\t')
+    df['marital-status'] = df['marital-status'].str.strip()
+    df['Gender'] = df['Gender'].str.strip()
+    df['relationship'] = df['relationship'].str.strip()
+    df.dropna()
+
+    df['Income'] = df['Income'].fillna(0)  # Turning Nan into the most Common Value
+    df.loc[df['Income'].str.contains('<=50K', na=False), 'Income'] = 0
+    df.loc[df['Income'].str.contains('>50K', na=False), 'Income'] = 1
+
+    # print(df['workType'].value_counts())  # = Private
+    df['workType'] = df['workType'].fillna("Private")  # Turning Nan into the most Common Value
+
+    return df
+
 
 def task2():
-    """TODO"""
+    df = cleanData2()
+    twoCols = df[['Pclass', 'Survived']].dropna()
+
+    groups = twoCols.groupby(['Pclass', 'Survived'])
+
+    print(groups.size())
+
+    # uniques = np.unique(twoCols['Pclass'])
+
+    attribute_value = 1
+
+    print('Entropy for', attribute_value)
+
+    sumVal1 = groups.size()[attribute_value].sum()
+    print(sumVal1, groups.size()[attribute_value])
+    # print(sumVal1, groups.size()[first_value])
+
+    pi = (groups.size()[attribute_value] / sumVal1)
+    # print(pi)
+    log2s = np.log2((groups.size()[attribute_value] / sumVal1))
+
+    entropies = log2s.multiply(-1 * pi)
+
+    print('Entropy', entropies.sum())
 
 
 def task3():
